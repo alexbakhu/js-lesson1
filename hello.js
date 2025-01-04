e987d476929629501fa3f478cdd4dc3d48c93901
alexbakhu <aleqsandrekepler123@gmail.com>
2025-01-04 14:58:22 +0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,155 @@
+let name1 = "mariami";
+
+let age1 = 20
+
+// es aris komentari javascriptshi.
+
+let name2 = "saba"
+
+let age2 = 15
+
+let color = "red"
+
+let y1 = 10
+
+let x1 = 20
+
+
+let z1 = 20 + 30
+ 
+let z2 = 20 * 30
+
+let z3 = 30 - 20
+
+let z4 = 10 / 5
+
+let z5 = y1 + x1
+
+console.log("hello")
+
+console.log("aleksandre bakhuashvili")
+
+console.log("10")
+
+console.log("black")
+
+console.log(z5)
+
+let num1 = 25
+
+let num2 = 30
+
+console.log(num1)
+
+console.log(num2)
+
+let num3 = num2 + num1
+
+console.log(num3)
+
+let num4 = 30 * 25
+
+let num5 = 30 - 25
+
+let num6 = 30 / 25
+console.log(num4)
+
+console.log(num5)
+
+console.log(num6)
+
+let num7 = 100
+
+console.log(num7)
+
+num7 = 200 
+
+console.log(num7)
+
+let age3 = 10
+
+age3 = 11
+
+console.log(age3)
+
+
+let p1 = document.querySelector(".p1")
+
+console.log(p1)
+
+p1.textContent= "aleksandre bakhuashvili"
+
+p1.style.color= "red"
+
+p1.style.background="black"
+
+p1.style.padding="20px"
+
+p1.style.borderRadius="20px"
+
+p1.style.width="200px"
+
+p1.style.height="300px"
+
+
+let p2 = document.querySelector(".p2")
+
+console.log(p2)
+
+
+p2.textContent= "paragraph."
+
+p2.style.color= "green"
+
+p2.style.background="orange"
+
+p2.style.padding="25px"
+
+p2.style.borderRadius="25px"
+
+p2.style.width="300px"
+
+p2.style.height="400px"
+
+p2.style.margin="50px"
+
+p2.style.border="10px solid blue"
+
+p2.style.display="flex"
+
+p2.style.justifyContent="center"
+
+
+
+let btn1 = document.querySelector(".btn1") 
+
+console.log(btn1)
+
+btn1.addEventListener("click", () => {
+    console.log("button was clicked");
+
+    btn1.style.background = "red"
+
+btn1.style.padding = "40px"
+})
+
+
+
+
+
+
+let btn2 = document.querySelector(".btn2") 
+
+console.log(btn2)
+
+btn2.addEventListener("click", () => {
+    console.log("button was clicked");
+
+    btn2.style.border = " 5px solid red"
+
+    btn2.style.width = "40px"
+    
+})
+
+
+
